f20d99cd48f45e65b568b3fcd567f9cf1aede146
Helmy138 <helmyafif714@gmail.com>
2024-10-04 10:42:06 +0700

    exercise day 3

---
+++ exercise/day3.ts
@@ -0,0 +1,52 @@
+// 1. git add .
+// 2. git commit -m "exercise day3"
+// 3. git push origin main
+
+
+
+// NO 1 //
+const n:number = 9;
+for(let i = 1; i <= 10; i++);
+console.log( y + " * " + j + " = " + " + num ");
+
+// NO 2 //
+const original : string = 'madam'
+const isPalindrom : boolean = original.split("").reverse().join("")
+console.log(isPalindrom);
+
+// NO 3 //
+const meter : number = 14000;
+const kilometer : number = meter / 1000;
+ 
+console.log(`${kilometer}km`);
+
+// NO 4 // 
+const harga: number = 10000
+const result: string = new Intl.NumberFormat(id.ID)
+
+
+// NO 5 //
+const input: string = 'hello world';
+const words = input.split(" ")
+console.log(words[0][0].toUpperCase() + words [0].slice(1));
+
+
+
+// NO 6 //
+const inputString: string = "hello world"
+
+
+
+// NO 7 //
+
+
+
+// NO 8 //
+
+
+
+
+
+// NO 9 //
+const num3: number = 42;
+const num4: number = 
